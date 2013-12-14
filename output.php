75370f51d1a4121c1125839e6a463d8e833684f0
Akshit Tripathi <tripathisoft@gmail.com>
2013-12-14 17:04:06 -0500

    Redoing Visuals
    Still need improvement. Work in progress. A suitable theme needs to be
    drafted after which work can be more streamlined.

--- output.php
+++ output.php
@@ -1,40 +1,47 @@
-<html>
+<!-- Code by Sunit Kumar Nandi -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./style.css">
+</head>
 
 <body>
-<?php
-$saving = $_REQUEST['saving'];
-if ($saving == 1){ 
-$data = $_POST['data'];
-$args = $_POST['args'];
-$stdin= $_POST['stdin'];
-$unique = rand(1, 10000);
-$file = "prog".$unique.".c";
-$srcpath = "./".$file;
-$executable = "./"."prog".$unique;
-$fp = fopen($file, "w") or die("<p>Couldn't open $file for writing!</p>");
-fwrite($fp, $data) or die("<p>Couldn't write values to file!</p>"); 
+	<?php
+	$saving = $_REQUEST['saving'];
+	if ($saving == 1){ 
+	$data = $_POST['data'];
+	$args = $_POST['args'];
+	$stdin= $_POST['stdin'];
+	$unique = rand(1, 10000);
+	$file = "prog".$unique.".c";
+	$srcpath = "./".$file;
+	$executable = "./"."prog".$unique;
+	$fp = fopen($file, "w") or die("<p>Couldn't open $file for writing!</p>");
+	fwrite($fp, $data) or die("<p>Couldn't write values to file!</p>"); 
 
-fclose($fp); 
-echo "<p>Saved to $file successfully!</p>";
+	fclose($fp); 
+	echo "<p>Saved to $file successfully!</p>";
 
-echo"<p>
-Compiling the program $file<br>
-Error list:<br>";
-$output1 = shell_exec("g++ $srcpath -o $executable 2>&1");
-echo "$output1";
-echo "</p>";
-echo "<p>Running the program:<br>";
-$output2 = shell_exec("echo $stdin | $executable $args");
-echo "$output2";
-echo "<br>
-</p>";
+	echo"<p>
+	Compiling the program $file<br>
+	Error list:<br>";
+	$output1 = shell_exec("g++ $srcpath -o $executable 2>&1");
+	echo "$output1";
+	echo "</p>";
+	echo "<p>Running the program:<br>";
+	$output2 = shell_exec("echo $stdin | $executable $args");
+	echo "$output2";
+	echo "<br>
+	</p>";
 
-shell_exec("rm -rf $srcpath $executable");
-}
-else {
-echo "<p>Enter your code in the left frame and hit run to display the output.</p>";
-}
-?>
+	shell_exec("rm -rf $srcpath $executable");
+	}
+	else {
+	echo "<p>Enter your code in the left frame and hit run to display the output.</p>";
+	}
+	?>
 </body>
 
 </html>
