390e036ed235ed47d8262bb38c08712b8d594887
Akshit Tripathi <tripathisoft@gmail.com>
2013-12-14 20:55:36 -0500

    Commit Level 2
    Made some minor changes.

--- textinput.php
+++ textinput.php
@@ -1,34 +1,36 @@
 <html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+
 <body>
-<?php
-$saving = $_REQUEST['saving'];
-if ($saving == 1){ 
-$data = $_POST['data'];
-$unique = rand(1, 10000);
-$file = "text".$unique.".txt";
+	<?php
+	$saving = $_REQUEST['saving'];
+	if ($saving == 1){ 
+		$data = $_POST['data'];
+		$unique = rand(1, 10000);
+		$file = "text".$unique.".txt";
 
-$fp = fopen($file, "w") or die("<p>Couldn't open $file for writing!</p>");
-fwrite($fp, $data) or die("<p>Couldn't write values to file!</p>"); 
+		$fp = fopen($file, "w") or die("<p>Couldn't open $file for writing!</p>");
+		fwrite($fp, $data) or die("<p>Couldn't write values to file!</p>"); 
 
-fclose($fp); 
-echo "<p>Saved to $file successfully! You may now use $file as a text input for your program.</p>";
-}
-?>
-
-<p>
-<form name="form1" method="post" action="?saving=1">
-Write your text here:
-<br>
-<textarea name="data" cols="50" rows="10">
-This is the file you can use to provide input to your program and later on open it inside your program to process the input.
-</textarea>
-<br>
-<input type="submit" value="Save">
-<br>
-<br>
-<a href="output.php">Click here to close text input</a>
-</form>
-</p>
+		fclose($fp); 
+		echo "<p>Saved to $file successfully! You may now use $file as a text input for your program.</p>";
+	}
+	?>
+	<p>
+		<form name="form1" method="post" action="?saving=1">
+		Write your text here:
+		<br>
+		<textarea name="data" cols="50" rows="10" placeholder="This is the file you can use to provide input to your program and later on open it inside your program to process the input."></textarea>
+		<br>
+		<button type="submit">Save</button>
+		<br>
+		<br>
+		<a href="output.php">Click here to close text input</a>
+		</form>
+	</p>
 
 </body>
 </html>
